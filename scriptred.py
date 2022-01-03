fc50634d6cf355eecec645a2f6acde2d680eb8bc
unknown <elangopriyanth@gmail.com>
2022-01-03 17:54:09 +0530

    enemy base tracking implemented

--- scriptred.py
+++ scriptred.py
@@ -3,7 +3,8 @@ from random import randint
 
 def ActRobot(robot):
         if robot.GetVirus() > 1000:
-                robot.DeployVirus(200)        
+                robot.DeployVirus(200)
+                
         return randint(1,4)
 
 
@@ -14,5 +15,5 @@ def ActBase(base):
     '''
     if base.GetElixir() > 500:
             base.create_robot('')
-
     return
+        
